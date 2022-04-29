191acfcfbd1643abe2227035062f07126c465d4b
Jaidynn Fohr <jaidynnfohr@mines.edu>
2022-04-29 16:35:04 -0600

    Changed Stores to Models on front end, deleted old files

---
+++ finalProjectFrontEnd/src/Brand.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+
+function Brand() {
+  const { brandId } = useParams();
+  const [singleBrand, setBrand] = useState({});
+
+  // Runs on page render or when brandId changes, but that happens on re-render
+  useEffect(() => {
+    fetch(`http://localhost:8000/brands/${brandId}`)
+      .then((body) => body.json())
+      .then((json) => setBrand(() => json));
+  }, [brandId]);
+  return (
+    <>
+      <h1>
+        {singleBrand.number} - {singleBrand.name}
+      </h1>
+      <Link to="models">Models</Link>
+    </>
+  );
+}
+
+export default Brand;
---
+++ finalProjectFrontEnd/src/Brands.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+function Brands() {
+  const [brands, setBrands] = useState([]);
+
+  // Analogous to ComponentDidMount; runs on component's mount and first render; runs once
+  useEffect(() => {
+    fetch(`http://localhost:8000/brands`)
+      .then((body) => body.json())
+      .then((json) => setBrands(() => [...json]));
+  }, []);
+
+  return (
+    <>
+      <h1>Brands</h1>
+      <ul>
+        {brands.map((brand) => (
+          <li key={brand._id}>
+            <Link to={`/brands/${brand._id}`}>{brand.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default Brands;
